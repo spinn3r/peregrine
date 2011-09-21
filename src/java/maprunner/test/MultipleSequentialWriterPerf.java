e5f1cda860f35fdab2ab9082779b0baf77dc4749
burtonator <devnull@localhost>
2011-09-21 12:36:14 -0700

    measure with more files

--- src/java/maprunner/test/MultipleSequentialWriterPerf.java
+++ src/java/maprunner/test/MultipleSequentialWriterPerf.java
@@ -17,7 +17,7 @@ public class MultipleSequentialWriterPerf {
         
         int max = 1;
         
-        for ( int i = 0; i <= 7; ++i ) {
+        for ( int i = 0; i <= 8; ++i ) {
 
             System.out.printf( "Writing to %s files.\n", max );
 
