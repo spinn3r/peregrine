7f57fe89d377013f81dcf3a2aaf39bb4c063bd81
burtonator <devnull@localhost>
2011-09-22 21:45:09 -0700

    .......

--- src/java/maprunner/shuffle/SortResult.java
+++ src/java/maprunner/shuffle/SortResult.java
@@ -72,22 +72,3 @@ public class SortResult {
     }
     
 }
-
-class IntermediateMerger extends SortMerger {
-    
-    public void merge( SortEntry entry, SortRecord record ) {
-        entry.values.addAll( ((SortEntry)record).values );
-    }
-
-    public SortEntry newSortEntry( SortRecord record ) {
-
-        SortEntry template = (SortEntry) record;
-        SortEntry result = new SortEntry();
-        result.keycmp = template.keycmp;
-        result.key = template.key;
-
-        return result;
-        
-    }
-
-}
