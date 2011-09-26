ff59162e8af11daea0b0965629d5f99e42f55b37
burtonator <devnull@localhost>
2011-09-25 19:38:10 -0700

    new build...

--- src/java/maprunner/Tuple.java
+++ src/java/maprunner/Tuple.java
@@ -4,51 +4,23 @@ import maprunner.util.*;
 import maprunner.keys.*;
 import maprunner.shuffle.SortRecord;
 
-public class Tuple implements SortRecord, Comparable {
+public class Tuple {
 
     public byte[] key = null;
     public byte[] value = null;
-
-    public long keycmp = -1;
 
     public Tuple( byte[] key, byte[] value ) {
 
         this.key = key;
         this.value = value;
-        this.keycmp = LongBytes.toLong( key );
         
     }
 
     public Tuple( Key key, Value value ) {
+
         this.key = key.toBytes();
         this.value = value.toBytes();
 
-        if ( key instanceof IntKey )
-            this.keycmp = ((IntKey)key).value;
-        
-    }
-
-    public int compareTo( Object obj ) {
-
-        //FIXME: remove this once I stop using Arrays.sort()
-        
-        long result = longValue() - ((Tuple)obj).longValue();
-
-        if ( result < 0 )
-            return -1;
-        else if ( result > 0 )
-            return 1;
-
-        return 0;
-
-    }
-
-    public long longValue() {
-        return keycmp;
-    }
-
-    public String toString() {
-        return String.format( "%s" , new IntKey( key ).value );
     }
 
 }
