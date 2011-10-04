47d331653ab19e73b03e12c639a158726bd890f6
burtonator <devnull@localhost>
2011-10-03 17:35:58 -0700

    init of dfsd support... GETs work which is nice.

---
+++ src/java/peregrine/dfsd/Main.java
@@ -0,0 +1,33 @@
+package peregrine.dfsd;
+
+import java.io.*;
+import java.util.*;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+
+public class Main {
+
+    public static int PORT = 11112;
+    
+    public static void main(String[] args) {
+
+        // Configure the server.
+        ServerBootstrap bootstrap = new ServerBootstrap(
+                new NioServerSocketChannelFactory(
+                        Executors.newCachedThreadPool(),
+                        Executors.newCachedThreadPool()));
+
+        // Set up the event pipeline factory.
+        bootstrap.setPipelineFactory( new NettyPipelineFactory() );
+
+        // Bind and start to accept incoming connections.
+        bootstrap.bind(new InetSocketAddress( PORT ));
+
+    }
+
+}
+    
