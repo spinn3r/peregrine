1db998f8f5621565025c4ecbd49ae943417c641c
burtonator <devnull@localhost>
2011-10-07 16:05:59 -0700

    new partition writer which does rolling and muli chunk writes....

---
+++ src/java/peregrine/io/partition/NewPartitionWriter.java
@@ -0,0 +1,121 @@
+package peregrine.io.partition;
+
+import java.io.*;
+import java.util.*;
+
+import peregrine.*;
+import peregrine.util.*;
+import peregrine.keys.*;
+import peregrine.values.*;
+import peregrine.io.chunk.*;
+
+/**
+ * 
+ */
+public class NewPartitionWriter implements PartitionWriter {
+
+    /**
+     * Chunk size for rollover files.
+     */
+    public static long CHUNK_SIZE = 134217728;
+
+    protected String path;
+
+    protected Partition partition;
+    
+    private int count = 0;
+
+    private List<PartitionWriterDelegate> partitionWriterDelegates;
+
+    private ChunkWriter chunkWriter = null;
+
+    private int chunk_id = 0;
+
+    public NewPartitionWriter( Partition partition,
+                               String path ) throws IOException {
+        this( partition, path, false );
+    }
+    
+    public NewPartitionWriter( Partition partition,
+                               String path,
+                               boolean append ) throws IOException {
+
+        this.path = path;
+        this.partition = partition;
+
+        Membership partitionMembership = Config.getPartitionMembership();
+
+        List<Host> hosts = partitionMembership.getHosts( partition );
+
+        partitionWriterDelegates = new ArrayList();
+
+        for( Host host : hosts ) {
+
+            // FIXME: for now make them ALL remote but we need logic to make them local
+            PartitionWriterDelegate delegate = new RemotePartitionWriterDelegate();
+
+            delegate.init( partition, host, path );
+
+            partitionWriterDelegates.add( delegate );
+            
+        }
+
+        /*
+        if ( append ) 
+            chunk_id = delegate.append();
+        else
+            delegate.erase();
+        */
+            
+        //create the first chunk...
+        rollover();
+        
+    }
+
+    @Override
+    public void write( byte[] key_bytes, byte[] value_bytes )
+        throws IOException {
+
+        chunkWriter.write( key_bytes, value_bytes );
+
+        rolloverWhenNecessary();
+        
+    }
+
+    @Override
+    public void close() throws IOException {
+        //close the last opened chunk writer...
+        chunkWriter.close();        
+    }
+
+    @Override
+    public String toString() {
+        return path;
+    }
+
+    private void rolloverWhenNecessary() throws IOException {
+
+        if ( chunkWriter.length() > CHUNK_SIZE )
+            rollover();
+        
+    }
+
+    private void rollover() throws IOException {
+
+        if ( chunkWriter != null )
+            chunkWriter.close();
+
+        List<ChunkWriter> writers = new ArrayList();
+
+        for ( PartitionWriterDelegate delegate : partitionWriterDelegates ) {
+            writers.add( new DefaultChunkWriter( delegate.newChunkWriter( chunk_id ) ) );
+        }
+        
+        chunkWriter = new MultiChunkWriter( writers );
+        
+        ++chunk_id; // change the chunk ID now for the next file.
+        
+    }
+
+}
+
