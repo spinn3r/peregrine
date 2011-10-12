9aad5940ccc8dc08a713b7027b68c038f6ffc6fa
burtonator <devnull@localhost>
2011-10-12 00:00:25 -0700

    added tests...

---
+++ src/test/peregrine/io/TestNewShuffleJobOutput.java
@@ -0,0 +1,75 @@
+package peregrine.io;
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
+
+import java.security.*;
+
+import peregrine.*;
+import peregrine.keys.*;
+import peregrine.values.*;
+import peregrine.util.*;
+import peregrine.shuffle.*;
+import peregrine.io.*;
+import peregrine.io.async.*;
+import peregrine.io.partition.*;
+import peregrine.io.chunk.*;
+import peregrine.pfsd.*;
+
+public class TestNewShuffleJobOutput extends peregrine.PFSTest {
+
+    public void setUp() {
+
+        Config.setHost( new Host( "localhost" ) );
+
+        Config.addPartitionMembership( 0, new Host( "localhost", 11112 ) );
+        Config.addPartitionMembership( 1, new Host( "localhost", 11113 ) );
+
+        new FSDaemon( 11112 );
+        new FSDaemon( 11113 );
+
+    }
+
+    /**
+     * test running with two lists which each have different values.
+     */
+    public void test1() throws Exception {
+
+        NewShuffleJobOutput output = new NewShuffleJobOutput();
+
+        ChunkReference chunkRef = new ChunkReference( new Partition( 0  ) );
+        chunkRef.local = 0;
+
+        output.onChunk( chunkRef );
+
+        for ( int i = 0; i < 100; ++i ) {
+
+            byte[] key = new StructWriter()
+                .writeVarint( i )
+                .toBytes()
+                ;
+
+            byte[] value = key;
+
+            output.emit( key, value );
+            
+        }
+
+        output.onChunkEnd( chunkRef );
+        
+    }
+
+    public static void main( String[] args ) throws Exception {
+
+        TestNewShuffleJobOutput t = new TestNewShuffleJobOutput();
+        t.setUp();
+        t.test1();
+        t.tearDown();
+
+        Thread.sleep( 2000L );
+        
+    }
+
+}
---
+++ src/test/peregrine/pfs/TestPipelineWrites.java
@@ -0,0 +1,99 @@
+package peregrine.pfs;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
+
+import java.security.*;
+
+import peregrine.*;
+import peregrine.keys.*;
+import peregrine.values.*;
+import peregrine.util.*;
+import peregrine.shuffle.*;
+import peregrine.io.*;
+import peregrine.io.async.*;
+import peregrine.io.partition.*;
+import peregrine.io.chunk.*;
+import peregrine.pfs.*;
+import peregrine.pfsd.*;
+
+public class TestPipelineWrites extends peregrine.BaseTest {
+
+    public void setUp() {
+
+        super.setUp();
+
+        new FSDaemon( String.format( "%s/localhost/11112", Config.PFS_ROOT ), 11112 );
+        new FSDaemon( String.format( "%s/localhost/11113", Config.PFS_ROOT ), 11113 );
+
+    }
+    
+    /**
+     * make sure the before and after 
+     */
+    public void test1() throws Exception {
+
+        URI uri = new URI( "http://localhost:11112/test-write-hash" );
+
+        RemoteChunkWriterClient client = new RemoteChunkWriterClient( uri );
+
+        client.setHeader( "X-pipeline", "localhost:11113" );
+        
+        int block = 16384;
+        
+        long max = 10 ;
+
+        long nr_bytes = (long)max * (long)block;
+        
+        System.out.printf( "Writing %,d bytes.\n" , nr_bytes );
+
+        MessageDigest digest = SHA1.getMessageDigest();
+        
+        for( int i = 0; i < max; ++i ) {
+
+            byte[] b = new byte[block];
+            
+            for( int j = 0; j < block ; ++j ) {
+                b[j] = (byte)i;
+                
+            }
+
+            digest.update( b );
+            
+            client.write( b );
+
+        }
+
+        System.out.printf( "Wrote all bytes.\n" );
+        
+        client.close();
+
+        byte[] digest_value = digest.digest();
+
+        File file = new File( String.format( "%s/localhost/11112/test-write-hash", Config.PFS_ROOT ) );
+
+        byte[] data = toByteArray( new FileInputStream( file ) );
+
+        String before = Hex.encode( digest_value );
+        String after  = Hex.encode( SHA1.encode( data ) );
+
+        System.out.printf( "before: %s\n", before );
+        System.out.printf( "after:  %s\n", after );
+
+        assertEquals( before , after );
+
+    }
+
+    public static void main( String[] args ) throws Exception {
+
+        TestPipelineWrites t = new TestPipelineWrites();
+        t.setUp();
+        t.test1();
+        t.tearDown();
+        
+    }
+
+}
