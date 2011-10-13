4c55c03ed3861e3c01fa7b263edda9415de5c59f
burtonator <devnull@localhost>
2011-10-12 23:45:06 -0700

    more unit tests....

---
+++ src/test/peregrine/TestNewReduceCode.java
@@ -0,0 +1,168 @@
+package peregrine;
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+import peregrine.*;
+import peregrine.io.*;
+import peregrine.keys.*;
+import peregrine.values.*;
+import peregrine.util.*;
+import peregrine.pagerank.*;
+import peregrine.io.partition.*;
+import peregrine.pfsd.*;
+
+public class TestNewReduceCode extends peregrine.BaseTest {
+
+    public static class Map extends Mapper {
+
+        @Override
+        public void map( byte[] key,
+                         byte[] value ) {
+
+            emit( key, value );
+            
+        }
+
+    }
+
+    public static class Reduce extends Reducer {
+
+        int count = 0;
+
+        int nth = 0;
+        
+        @Override
+        public void reduce( byte[] key, List<byte[]> values ) {
+
+            ++count;
+
+            List<Integer> ints = new ArrayList();
+
+            for( byte[] val : values ) {
+                ints.add( IntBytes.toInt( val ) );
+            }
+            
+            // full of fail... 
+            if ( values.size() != 2 )
+                throw new RuntimeException( String.format( "%s does not equal %s (%s) on nth reduce %s" , values.size(), 2, ints, nth ) );
+
+            ++nth;
+            
+        }
+
+        @Override
+        public void cleanup() {
+
+            if ( count == 0 )
+                throw new RuntimeException();
+            
+        }
+
+    }
+
+    protected Config config;
+
+    protected List<FSDaemon> daemons = new ArrayList();
+    
+    public void setUp() {
+
+        super.setUp();
+        
+        Config config0 = newConfig( "localhost", 11112 );
+        //Config config1 = newConfig( "localhost", 11113 );
+
+        daemons.add( new FSDaemon( config0 ) );
+        //new FSDaemon( config1 );
+
+        config = config0;
+        
+    }
+
+    private Config newConfig( String host, int port ) {
+
+        Config config = new Config( host, port );
+
+        config.addPartitionMembership( 0, new Host( "localhost", 11112 ) );
+        //config.addPartitionMembership( 1, new Host( "localhost", 11113 ) );
+
+        return config;
+        
+    }
+    
+    public void test1() throws Exception {
+
+        String path = String.format( "/test/%s/test1.in", getClass().getName() );
+
+        DefaultPartitionWriter.CHUNK_SIZE = 2048;
+        
+        ExtractWriter writer = new ExtractWriter( config, path );
+
+        int max = 10000;
+        
+        for( int i = 0; i < max; ++i ) {
+
+            byte[] key = new IntKey( i ).toBytes();
+            byte[] value = key;
+            writer.write( key, value );
+            
+        }
+
+        for( int i = 0; i < max; ++i ) {
+
+            byte[] key = new IntKey( i ).toBytes();
+            byte[] value = key;
+            writer.write( key, value );
+            
+        }
+
+        writer.close();
+
+        // int count = 0;
+        
+        // //make sure too many values weren't written.
+        // Membership membership = config.getPartitionMembership();
+
+        // for( Partition part : membership.getPartitions() ) {
+
+        //     for( Host host : membership.getHosts( part ) ) {
+
+        //         LocalPartitionReader reader = new LocalPartitionReader( config, part, host, path );
+
+        //         while( reader.hasNext() ) {
+
+        //             byte[] key = reader.key();
+        //             byte[] value = reader.value();
+
+        //             ++count ;
+                    
+        //         }
+                
+        //     }
+            
+        // }
+
+        // //FIXME: read the value off BOTH partitions / hosts.
+        // assertTrue( count > 0 );
+        
+        String output = String.format( "/test/%s/test1.out", getClass().getName() );
+
+        Controller controller = new Controller( config );
+        
+        controller.map( Map.class, path );
+
+        for( FSDaemon daemon : daemons ) {
+            daemon.shufflerFactory.closeAll();
+        }
+
+    }
+
+    public static void main( String[] args ) throws Exception {
+        TestNewReduceCode test = new TestNewReduceCode();
+        test.setUp();
+        test.test1();
+        
+    }
+
+}
---
+++ src/test/peregrine/TestPutRequests.java
@@ -0,0 +1,92 @@
+package peregrine;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+import peregrine.*;
+import peregrine.io.*;
+import peregrine.keys.*;
+import peregrine.values.*;
+import peregrine.util.*;
+import peregrine.pagerank.*;
+import peregrine.io.partition.*;
+import peregrine.pfs.*;
+import peregrine.pfsd.*;
+
+public class TestPutRequests extends peregrine.BaseTest {
+
+    protected Config config;
+
+    protected List<FSDaemon> daemons = new ArrayList();
+    
+    public void setUp() {
+
+        super.setUp();
+        
+        config = newConfig( "localhost", 11112 );
+
+        daemons.add( new FSDaemon( config ) );
+
+    }
+
+    private Config newConfig( String host, int port ) {
+
+        Config config = new Config( host, port );
+
+        config.addPartitionMembership( 0, new Host( "localhost", 11112 ) );
+
+        return config;
+        
+    }
+    
+    public void test1() throws Exception {
+
+        int max = 1000;
+        
+        for( int i = 0; i < max; ++i ) {
+
+            System.out.printf( "Writing %,d out of %,d\n", i , max );
+            
+            RemoteChunkWriterClient client = new RemoteChunkWriterClient( new URI( String.format( "http://localhost:11112/foo/%s", i ) ) );
+
+            client.write( "xxxxxxxxxxxxxxxxxx".getBytes() );
+
+            client.close();
+            
+        }
+        
+    }
+
+    public void test2() throws Exception {
+
+        int max = 10;
+        
+        for( int i = 0; i < max; ++i ) {
+
+            System.out.printf( "Writing %,d out of %,d\n", i , max );
+            
+            RemoteChunkWriterClient client = new RemoteChunkWriterClient( new URI( String.format( "http://localhost:11112/foo/%s", i ) ) );
+
+            client.write( "xxxxxxxxxxxxxxxxxx".getBytes() );
+
+            Thread.sleep( 1000L );
+            
+            client.write( "xxxxxxxxxxxxxxxxxx".getBytes() );
+
+            client.close();
+            
+        }
+        
+    }
+
+    public static void main( String[] args ) throws Exception {
+        TestPutRequests test = new TestPutRequests();
+        test.setUp();
+        test.test1();
+        //test.test2queue.();
+        
+    }
+
+}
