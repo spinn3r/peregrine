5d7d5b59da0c2664af6e5281c20674a0a8da5834
burtonator <devnull@localhost>
2011-10-15 20:03:10 -0700

    load config

--- src/java/peregrine/pfsd/Main.java
+++ src/java/peregrine/pfsd/Main.java
@@ -12,27 +12,41 @@ import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 
 import peregrine.*;
 
+import com.spinn3r.log5j.Logger;
+
 public class Main {
+
+    private static final Logger log = Logger.getLogger();
 
     public static void main(String[] args) throws Exception {
 
         // init log4j ... 
         org.apache.log4j.xml.DOMConfigurator.configure( "conf/log4j.xml" );
 
-        String dir = Config.DEFAULT_ROOT;
-        int port = FSDaemon.PORT;
+        Properties props = new Properties();
+        props.load( new FileInputStream( "conf/peregrine.conf" ) );
+
+        String root = props.get( "root" ).toString();
+        int port    = Integer.parseInt( props.get( "port" ).toString() );
 
         if ( args.length == 2 ) {
 
-            dir = args[0];
+            root = args[0];
             port = Integer.parseInt( args[1] );
             
         }
 
+        String hostname = System.getenv( "HOSTNAME" );
+
+        if ( hostname == null )
+            hostname = "localhost";
+        
+        log.info( "Starting on %s on port %s" , hostname, port );
+        
         Config config = new Config();
 
-        config.setRoot( dir );
-        config.setHost( new Host( "localhost", port ) );
+        config.setRoot( root );
+        config.setHost( new Host( hostname, port ) );
         
         new FSDaemon( config );
 
