50d3137b688e79483586f0b5050e3c76becfb44d
burtonator <devnull@localhost>
2011-10-18 11:35:33 -0700

    flush callable in dedicated class

--- src/java/peregrine/shuffle/receiver/ShuffleReceiver.java
+++ src/java/peregrine/shuffle/receiver/ShuffleReceiver.java
@@ -131,23 +131,3 @@ public class ShuffleReceiver {
     }
 
 }
-
-class ShuffleReceiverFlushCallable implements Callable {
-
-    private ShuffleOutputWriter writer;
-    
-    ShuffleReceiverFlushCallable( ShuffleOutputWriter writer ) {
-        this.writer = writer;
-    }
-
-    @Override
-    public Object call() throws Exception {
-
-        // close this in a background task since this blocks.
-        this.writer.close();
-        
-        return null;
-        
-    }
-    
-}
