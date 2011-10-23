76039ece40623d86788e1bb657905d20f731c8d6
burtonator <devnull@localhost>
2011-10-23 16:52:01 -0700

    one daemon

---
+++ src/test/peregrine/BaseTestWithOneDaemon.java
@@ -0,0 +1,69 @@
+package peregrine;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
+
+import java.security.*;
+
+import peregrine.*;
+import peregrine.keys.*;
+import peregrine.values.*;
+import peregrine.util.*;
+import peregrine.reduce.*;
+import peregrine.io.*;
+import peregrine.io.async.*;
+import peregrine.io.partition.*;
+import peregrine.io.chunk.*;
+import peregrine.pfs.*;
+import peregrine.pfsd.*;
+
+public class BaseTestWithOneDaemon extends peregrine.BaseTest {
+
+    protected Host controller;
+
+    protected Config config;
+
+    protected Config config0;
+
+    protected List<FSDaemon> daemons = new ArrayList();
+    
+    public void setUp() {
+
+        super.setUp();
+
+        controller = new Host( "localhost", 11111 );
+
+        config = newConfig( "localhost", 11111 );
+        
+        config0 = newConfig( "localhost", 11112 );
+
+        daemons.add( new FSDaemon( config0 ) );
+
+    }
+
+    protected Config newConfig( String host, int port ) {
+
+        Config config = new Config( host, port );
+
+        config.setController( controller );
+        
+        config.addMembership( 0, new Host( "localhost", 11112 ) );
+
+        return config;
+        
+    }
+
+    public void tearDown() {
+
+        for( FSDaemon daemon : daemons ) {
+            daemon.shutdown();
+        }
+        
+        super.tearDown();
+
+    }
+
+}
