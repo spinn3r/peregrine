13ae93133ac270a43d032055a379d4856c262ec1
burtonator <devnull@localhost>
2011-10-23 17:17:20 -0700

    some cleanup...

--- src/java/peregrine/reduce/SortEntry.java
+++ src/java/peregrine/reduce/SortEntry.java
@@ -14,23 +14,11 @@ import peregrine.map.*;
 public final class SortEntry {
 
     public byte[] key;
-    
-    private long keycmp;
-
-    private int len = 0;
 
     private List<byte[]> values = new ArrayList();
 
     public SortEntry( byte[] key ) {
-
-        //FIXME: this must be removed because it is not efficient.
-        this.keycmp = LongBytes.toLong( key );
         this.key = key;
-        
-    }
-
-    public long cmp( SortEntry entry ) {
-        return keycmp - entry.keycmp;
     }
 
     public void addValue( byte[] value ) {
