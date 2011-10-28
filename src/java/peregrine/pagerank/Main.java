4ac5f09b05178042c8e96d84711c15a901ceaa7e
burtonator <devnull@localhost>
2011-10-28 11:23:16 -0700

    ...

--- src/java/peregrine/pagerank/Main.java
+++ src/java/peregrine/pagerank/Main.java
@@ -16,9 +16,15 @@ public class Main {
 
     public static void main( String[] args ) throws Exception {
 
-        int nr_nodes = Integer.parseInt( args[0] );
-        int max_edges_per_node = Integer.parseInt( args[1] );
+        // use some conservative defaults with no args.
+        int nr_nodes = 500;
+        int max_edges_per_node = 10;
 
+        if ( args.length == 2 ) {
+            nr_nodes = Integer.parseInt( args[0] );
+            max_edges_per_node = Integer.parseInt( args[1] );
+        }
+        
         System.out.printf( "Running with nr_nodes: %,d , max_edges_per_node: %,d\n", nr_nodes, max_edges_per_node );
         
         DOMConfigurator.configure( "conf/log4j.xml" );
