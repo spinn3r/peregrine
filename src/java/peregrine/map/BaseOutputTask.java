f222df3db588a61f2694be477783bd15b2753a5a
burtonator <devnull@localhost>
2011-10-28 13:36:42 -0700

    log job output

--- src/java/peregrine/map/BaseOutputTask.java
+++ src/java/peregrine/map/BaseOutputTask.java
@@ -67,6 +67,10 @@ public abstract class BaseOutputTask {
 
         this.jobOutput = JobOutputFactory.getJobOutput( config, partition, output );
 
+        for( int i = 0; i < jobOutput.length; ++i ) {
+            log.info( "Job output %,d is %s" , i , jobOutput[i] ):
+        }
+        
     }
 
     public void teardown() throws IOException {
