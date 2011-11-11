f12aa43cc55b7b39ed618b131d2d8345637a625b
burtonator <devnull@localhost>
2011-11-10 16:58:26 -0800

    make it test multiple configs

--- src/test/peregrine/TestPagerank.java
+++ src/test/peregrine/TestPagerank.java
@@ -3,58 +3,10 @@ package peregrine;
 import peregrine.io.*;
 import peregrine.pagerank.*;
 
-public class TestPagerank extends peregrine.BaseTestWithMultipleDaemons {
+public class TestPagerank extends peregrine.BaseTestWithMultipleConfigs {
 
-    public TestPagerank() {
-        super( 1, 2, 5 );
-    }
-
-    public void test1() throws Exception {
-
-        // only 0 and 1 should be dangling.
-
-/*        
-        String path = "/pr/test.graph";
-        
-        ExtractWriter writer = new ExtractWriter( config, path );
-
-        GraphBuilder.addRecord( writer, 2, 0, 1 );
-        GraphBuilder.addRecord( writer, 3, 1, 2 );
-        GraphBuilder.addRecord( writer, 4, 2, 3 );
-        GraphBuilder.addRecord( writer, 5, 2, 3 );
-        GraphBuilder.addRecord( writer, 6, 2, 3 );
-        GraphBuilder.addRecord( writer, 7, 2, 3 );
-        GraphBuilder.addRecord( writer, 8, 2, 3 );
-        GraphBuilder.addRecord( writer, 9, 2, 3 );
-        GraphBuilder.addRecord( writer, 10, 2, 3 );
-        GraphBuilder.addRecord( writer, 11, 2, 3 );
-
-        writer.close();
-
-        new Pagerank( config ).exec( path );
-
-*/        
-        
-    }
-
-    private void doRandomTest( int nr_nodes,
-                               int max_edges_per_node ) throws Exception {
-
-        // only 0 and 1 should be dangling.
-
-        String path = "/pr/test.graph";
-
-        ExtractWriter writer = new ExtractWriter( config, path );
-
-        GraphBuilder.buildRandomGraph( writer, nr_nodes , max_edges_per_node );
-
-        writer.close();
-        
-        new Pagerank( config ).exec( path );
-
-    }
-    
-    public void test2() throws Exception {
+    @Override
+    public void doTest() throws Exception {
 
         doRandomTest( 50000 , 100 );
         //doRandomTest( 500, 10 );
@@ -75,6 +27,23 @@ public class TestPagerank extends peregrine.BaseTestWithMultipleDaemons {
         // doRandomTest( 2000, 100 );
         // doRandomTest( 3000, 100 );
         // doRandomTest( 4000, 100 );
+
+    }
+
+    private void doRandomTest( int nr_nodes,
+                               int max_edges_per_node ) throws Exception {
+
+        // only 0 and 1 should be dangling.
+
+        String path = "/pr/test.graph";
+
+        ExtractWriter writer = new ExtractWriter( config, path );
+
+        GraphBuilder.buildRandomGraph( writer, nr_nodes , max_edges_per_node );
+
+        writer.close();
+        
+        new Pagerank( config ).exec( path );
 
     }
 
