55a9a2f83de715df0bb2918fbfa0b46093dd9aad
burtonator <devnull@localhost>
2011-11-18 13:22:49 -0800

    I can't easily duplicate this bug

--- src/test/peregrine/BaseTestWithMultipleConfigs.java
+++ src/test/peregrine/BaseTestWithMultipleConfigs.java
@@ -14,16 +14,15 @@ public abstract class BaseTestWithMultipleConfigs extends peregrine.BaseTest {
     //FIXME: does not yet work with 3 replicas 
     //public static int[] REPLICAS     = new int[] { 1, 2, 3 };
 
-    /*
     public static int[] CONCURRENCY  = new int[] { 1, 2, 4, 8 };
     public static int[] REPLICAS     = new int[] { 1, 2 };
     public static int[] HOSTS        = new int[] { 1, 2, 4, 8 };
-    */
 
+    /*
     public static int[] CONCURRENCY  = new int[] { 4 };
     public static int[] REPLICAS     = new int[] { 1 };
     public static int[] HOSTS        = new int[] { 8 };
-    
+    */
 
     protected Host controller;
 
