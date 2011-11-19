9bf58ac6b067710e8d612c15a425f52ea315cb6f
burtonator <devnull@localhost>
2011-11-19 15:10:01 -0800

    direct shuffler

---
+++ src/java/peregrine/shuffle/sender/ShuffleJobOutputDelegate.java
@@ -0,0 +1,17 @@
+package peregrine.shuffle.sender;
+
+import java.io.*;
+import java.util.concurrent.*;
+
+import peregrine.config.*;
+import peregrine.io.*;
+import peregrine.io.partition.*;
+import peregrine.util.*;
+import peregrine.io.chunk.*;
+import com.spinn3r.log5j.Logger;
+
+public interface ShuffleJobOutputDelegate extends JobOutput, LocalPartitionReaderListener {
+
+    public void emit( int to_partition, byte[] key , byte[] value );
+
+}
