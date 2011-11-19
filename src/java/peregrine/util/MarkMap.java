4d29d23fe36f5e733516616529f851ea1c93f9d5
burtonator <devnull@localhost>
2011-11-18 16:42:30 -0800

    added the differential updates back in

--- src/java/peregrine/util/MarkMap.java
+++ src/java/peregrine/util/MarkMap.java
@@ -15,17 +15,23 @@ public class MarkMap<T,V> {
 
     protected void put( T entry, V value ) {
 
+        boolean updated = map.get( entry ) == null;
+        
 		map.put( entry, value );
 
-        fireUpdated( entry, MarkListener.Status.MARKED );
+        if ( updated ) 
+            fireUpdated( entry, MarkListener.Status.MARKED );
         
     }
     
 	public void clear( T entry ) {
 
+        boolean updated = map.get( entry ) != null;
+
         map.remove( entry );
 
-        fireUpdated( entry, MarkListener.Status.CLEARED );
+        if ( updated ) 
+            fireUpdated( entry, MarkListener.Status.CLEARED );
         
 	}
 
