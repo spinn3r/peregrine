35ecd4f64573c18fad19d89fd04317e2ea9b5e15
burtonator <devnull@localhost>
2011-11-18 16:39:15 -0800

    further reduce memory

--- bin/start-all.sh
+++ bin/start-all.sh
@@ -4,7 +4,7 @@ export BRANCH=burton-bench
 export DIR=/root/peregrine
 export LOGDIR=/var/log
 export MAX_MEMORY=288M
-export MAX_DIRECT_MEMORY=900M
+export MAX_DIRECT_MEMORY=800M
 export HOSTNAME=$(hostname)
 
 start() {
