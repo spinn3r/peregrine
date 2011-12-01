1e978520a8279f56972a5995b05ff9588a0fff4e
burtonator <devnull@localhost>
2011-11-30 17:24:44 -0800

    enable mlock again

--- src/java/peregrine/shuffle/ShuffleInputReader.java
+++ src/java/peregrine/shuffle/ShuffleInputReader.java
@@ -16,7 +16,7 @@ import org.jboss.netty.buffer.*;
  */
 public class ShuffleInputReader {
 
-    public static boolean ENABLE_MEMLOCK = false;
+    public static boolean ENABLE_MEMLOCK = true;
     
     /**
      * The current packet index we're on.
