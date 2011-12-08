63883eb7f970376cc20f539c8ef8a26577b9c80a
burtonator <devnull@localhost>
2011-12-07 16:21:23 -0800

    wrong arg

--- src/java/peregrine/sysstat/Main.java
+++ src/java/peregrine/sysstat/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     public static void main( String[] args ) throws Exception {
 
-        String dev = args[1];
+        String dev = args[0];
         
         LinuxPlatform platform = new LinuxPlatform( dev );
 
