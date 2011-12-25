1a45388bb76bcfe9539318f1bfe6f11db16a70db
burtonator <devnull@localhost>
2011-12-24 16:47:05 -0800

    print all the suppressed exceptions

--- src/java/peregrine/io/util/GroupIOException.java
+++ src/java/peregrine/io/util/GroupIOException.java
@@ -4,7 +4,8 @@ import java.util.*;
 import java.io.*;
 
 /**
- * Implements JDK 1.7 try-with-resources style closing for multiple Closeables.
+ * Implements JDK 1.7 try-with-resources style closing for multiple Closeables /
+ * Flushables with an exception that includes all exceptions.
  *
  * http://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
  */
@@ -21,6 +22,26 @@ public class GroupIOException extends IOException {
     
     public void addSuppressed( Throwable t ) {
         suppressed.add( t );
+    }
+
+    public void printStackTrace( PrintStream out ) {
+
+        cause.printStackTrace( out );
+
+        for ( Throwable current : suppressed ) {
+            current.printStackTrace( out );
+        }
+        
+    }
+
+    public void printStackTrace( PrintWriter out ) {
+
+        cause.printStackTrace( out );
+
+        for ( Throwable current : suppressed ) {
+            current.printStackTrace( out );
+        }
+
     }
     
 }
