0a49a7bbff42d11d582a0de709cd462d78bd022c
burtonator <devnull@localhost>
2011-12-27 03:22:03 -0800

    ...

--- src/java/peregrine/app/pagerank/NodeIndegreeJob.java
+++ src/java/peregrine/app/pagerank/NodeIndegreeJob.java
@@ -1,7 +1,9 @@
 package peregrine.app.pagerank;
 
 import java.util.*;
+
 import peregrine.*;
+import peregrine.io.*;
 import peregrine.util.*;
 
 public class NodeIndegreeJob {
@@ -23,15 +25,26 @@ public class NodeIndegreeJob {
 
     public static class Reduce extends Reducer {
 
+        JobOutput nodeIndegreeOutput         = null;
+        JobOutput graphBySourceOutput        = null;
+
+        @Override
+        public void init( List<JobOutput> output ) {
+            nodeIndegreeOutput   = output.get(0);
+            graphBySourceOutput  = output.get(1);
+        }
+
         @Override
         public void reduce( StructReader key, List<StructReader> values ) {
             
             int indegree = values.size();
 
-            emit( key, new StructWriter()
-            		       .writeInt( indegree )
-            		       .toStructReader() );
-            
+            nodeIndegreeOutput.emit( key, StructReaders.wrap( indegree ) );
+
+            //now emit these to graphBySource so that we have a graph which is
+            //sorted (and we are 100% certain it is sorted).
+            graphBySourceOutput.emit( key, StructReaders.wrap( values ) );
+
         }
         
     }
