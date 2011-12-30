bf566ff1451cdeb01e2d1c945196ca1825a78fcc
burtonator <devnull@localhost>
2011-12-29 22:25:05 -0800

    disable prefetch logging

--- src/java/peregrine/util/netty/PrefetchReader.java
+++ src/java/peregrine/util/netty/PrefetchReader.java
@@ -52,7 +52,7 @@ public class PrefetchReader implements Closeable {
      */
     public static long DEFAULT_CAPACITY = DEFAULT_PAGE_SIZE * 4;
 
-    public static boolean DEFAULT_ENABLE_LOG = true;
+    public static boolean DEFAULT_ENABLE_LOG = false;
     
     protected long pageSize = DEFAULT_PAGE_SIZE;
 
