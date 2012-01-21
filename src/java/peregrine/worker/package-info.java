ea95ef6cdcac0145dcf1a21835fa157dbab98ec4
burtonator <devnull@localhost>
2012-01-21 14:17:41 -0800

    ....

--- src/java/peregrine/worker/package-info.java
+++ src/java/peregrine/worker/package-info.java
@@ -1,6 +1,6 @@
 
 /**
- * PFS (partitioned filesystem) support as well as main daemon interface (RPC, etc).
+ * Worker daemon including PFS (partitioned filesystem) support as well as main daemon interface (RPC, etc).
  * 
  * <p>
  * <img src="http://i.imgur.com/O59Dx.png"/> <br/> 
