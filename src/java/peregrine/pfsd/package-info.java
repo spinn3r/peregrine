44e01c592ccec23f88e12d8495b8590b3909579b
burtonator <devnull@localhost>
2012-01-21 14:11:45 -0800

    removed pfsd

--- src/java/peregrine/pfsd/package-info.java
+++
@@ -1,8 +0,0 @@
-
-/**
- * PFS (partitioned filesystem) support as well as main daemon interface (RPC, etc).
- * 
- * <p>
- * <img src="http://i.imgur.com/O59Dx.png"/> <br/> 
- */
-package peregrine.pfsd;
