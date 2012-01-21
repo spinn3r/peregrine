0a1b222ae1847f0e9c09ffb02700666934a8d43f
burtonator <devnull@localhost>
2012-01-20 17:53:58 -0800

    forgot to remove test failure code

--- src/test/peregrine/TestExampleDriver.java
+++ src/test/peregrine/TestExampleDriver.java
@@ -48,9 +48,6 @@ public class TestExampleDriver extends peregrine.BaseTestWithTwoDaemons {
     }
         
     public void test1() throws Exception {
-
-        if ( true )
-            throw new RuntimeException( "FIXME" );
         
         //register the driver
         IODriverRegistry.register( new ExampleIODriver() );
