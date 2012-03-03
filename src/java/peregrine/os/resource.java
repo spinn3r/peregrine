650cf2dd1a048ce0b747a83710c94d6be02787fa
burtonator <devnull@localhost>
2012-03-03 12:18:17 -0800

    more setrlimit work... more constants defined...

--- src/java/peregrine/os/resource.java
+++ src/java/peregrine/os/resource.java
@@ -27,11 +27,84 @@ public class resource {
     private static InterfaceDelegate delegate
         = (InterfaceDelegate)Native.loadLibrary( "c", InterfaceDelegate.class); 
 
+    /** 
+
+      The following code will print out these constants on Linux:
+      
+        #include <sys/time.h>
+        #include <sys/resource.h>
+        #include <stdio.h>
+
+        int main() {
+            printf( "RLIMIT_AS: %d\n",       RLIMIT_AS );
+            printf( "RLIMIT_MEMLOCK: %d\n",  RLIMIT_MEMLOCK );
+            printf( "RLIMIT_RSS: %d\n",      RLIMIT_RSS );
+            printf( "RLIMIT_NOFILE: %d\n",   RLIMIT_NOFILE );
+        }
+
+      On Linux these values are:
+
+        RLIMIT_AS: 9
+        RLIMIT_MEMLOCK: 8
+        RLIMIT_RSS: 5
+        RLIMIT_NOFILE: 7
+
+      But on OS X these values are:
+
+        RLIMIT_AS: 5
+        RLIMIT_MEMLOCK: 6
+        RLIMIT_RSS: 5
+        RLIMIT_NOFILE: 8
+
+      TODO: find some way we can have this work on ALL platforms without having
+      to change the constants.
+        
+    */
+
     /**
-     * FIXME: This works but on OS X this is defined as 8 and on Linux it's 7 ... WTF
-     * that isn't helpful.
+     * Specifies a value one greater than the maximum file descriptor number
+     * that can be opened by this process. Attempts (open(2), pipe(2), dup(2),
+     * etc.) to exceed this limit yield the error EMFILE. (Historically, this
+     * limit was named RLIMIT_OFILE on BSD.)
      */
-    public static final int RLIMIT_NOFILE = 7; /* max number of open files */
+    public static final int RLIMIT_NOFILE = 7; 
+
+    /**
+     * The maximum number of bytes of memory that may be locked into RAM. In
+     * effect this limit is rounded down to the nearest multiple of the system
+     * page size. This limit affects mlock(2) and mlockall(2) and the mmap(2)
+     * MAP_LOCKED operation. Since Linux 2.6.9 it also affects the shmctl(2)
+     * SHM_LOCK operation, where it sets a maximum on the total bytes in shared
+     * memory segments (see shmget(2)) that may be locked by the real user ID of
+     * the calling process. The shmctl(2) SHM_LOCK locks are accounted for
+     * separately from the per-process memory locks established by mlock(2),
+     * mlockall(2), and mmap(2) MAP_LOCKED; a process can lock bytes up to this
+     * limit in each of these two categories. In Linux kernels before 2.6.9,
+     * this limit controlled the amount of memory that could be locked by a
+     * privileged process. Since Linux 2.6.9, no limits are placed on the amount
+     * of memory that a privileged process may lock, and this limit instead
+     * governs the amount of memory that an unprivileged process may lock.
+     */
+    public static final int RLIMIT_MEMLOCK = 8; 
+
+    /**
+     * Specifies the limit (in pages) of the process's resident set (the number
+     * of virtual pages resident in RAM). This limit only has effect in Linux
+     * 2.4.x, x < 30, and there only affects calls to madvise(2) specifying
+     * MADV_WILLNEED.
+     */
+    public static final int RLIMIT_RSS = 5; 
+
+    /**
+     * The maximum size of the process's virtual memory (address space) in
+     * bytes. This limit affects calls to brk(2), mmap(2) and mremap(2), which
+     * fail with the error ENOMEM upon exceeding this limit. Also automatic
+     * stack expansion will fail (and generate a SIGSEGV that kills the process
+     * if no alternate stack has been made available via sigaltstack(2)). Since
+     * the value is a long, on machines with a 32-bit long either this limit is
+     * at most 2 GiB, or this resource is unlimited.
+     */
+    public static final int RLIMIT_AS = 9; 
 
     public static class Rlimit extends Structure {
 
