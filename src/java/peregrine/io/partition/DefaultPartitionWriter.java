807066074a2776381956bbf0c98e60cf481852f7
burtonator <devnull@localhost>
2012-03-24 21:41:01 -0700

    revert ot see if this fixes the build

--- src/java/peregrine/io/partition/DefaultPartitionWriter.java
+++ src/java/peregrine/io/partition/DefaultPartitionWriter.java
@@ -98,9 +98,18 @@ public class DefaultPartitionWriter implements PartitionWriter, ChunkWriter {
 
             log.info( "Creating partition writer delegate for host: " + host );
             
-            PartitionWriterDelegate delegate
-                = new RemotePartitionWriterDelegate( config, autoSync );
+            //PartitionWriterDelegate delegate
+            //    = new RemotePartitionWriterDelegate( config, autoSync );
 
+            PartitionWriterDelegate delegate;
+            
+            if ( host.equals( config.getHost() ) ) {
+                delegate = new LocalPartitionWriterDelegate( config,  autoSync );
+            } else { 
+                delegate = new RemotePartitionWriterDelegate( config, autoSync );
+            }
+
+            
             delegate.init( config, partition, host, path );
 
             partitionWriterDelegates.add( delegate );
