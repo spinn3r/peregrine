6f43974d7b511b79e71e5b11785f49e15d82c33e
burtonator <devnull@localhost>
2012-05-27 21:51:11 -0700

    ...

--- src/java/peregrine/reduce/ReduceRunner.java
+++ src/java/peregrine/reduce/ReduceRunner.java
@@ -178,7 +178,7 @@ public class ReduceRunner {
 
             new Closer( merger ).close();
             
-            //new Closer( prefetchReader ).close();
+            new Closer( prefetchReader ).close();
             
             //new Closer( merger, prefetchReader ).close();
 
