6d2ed865854fd0dc99e52a657264a6fc247d0d53
burtonator <devnull@localhost>
2012-05-27 19:57:25 -0700

    ...

--- src/java/peregrine/reduce/ReduceRunner.java
+++ src/java/peregrine/reduce/ReduceRunner.java
@@ -165,7 +165,7 @@ public class ReduceRunner {
 
             SystemProfiler profiler = config.getSystemProfiler();
 
-            prefetchReader = createPrefetchReader( readers );
+            //prefetchReader = createPrefetchReader( readers );
 
             merger = new ChunkMerger( task, listener, partition, readers, jobOutput );
         
@@ -174,7 +174,7 @@ public class ReduceRunner {
             log.info( "Merged with profiler rate: \n%s", profiler.rate() );
 
         } finally {
-            //FIXME: new Closer( prefetchReader, merger ).close();
+            new Closer( prefetchReader, merger ).close();
         }
         
     }
