3774c4977a318feea71d8dd104666210dff16097
burtonator <devnull@localhost>
2012-05-29 15:48:23 -0700

    mode

--- src/java/peregrine/app/wikirank/Main.java
+++ src/java/peregrine/app/wikirank/Main.java
@@ -20,6 +20,11 @@ import peregrine.config.*;
 import peregrine.util.*;
 import peregrine.worker.*;
 
+/**
+ * Main binary for running peregrine on wikipedia graph data.
+ *
+ * ./bin/jexec peregrine.app.wikirank.Main --nodes_path=/d0/enwiki-20120502-page.sql --links_path=/d0/enwiki-20120502-pagelinks.sql
+ */
 public class Main {
 
     public static void main( String[] args ) throws Exception {
@@ -31,8 +36,23 @@ public class Main {
 
         String nodes_path = getopt.getString( "nodes_path" );
         String links_path = getopt.getString( "links_path" );
-        
-        new Wikirank( config, nodes_path, links_path ).exec();
+
+        String stage = getopt.getString( "stage", "all" );
+
+        Wikirank wikirank = new Wikirank( config, nodes_path, links_path );
+
+        if ( "init".equals( stage ) ) {
+            wikirank.init();
+        }
+
+        if ( "exec".equals( stage ) ) {
+            wikirank.exec();
+        }
+
+        if ( "all".equals( stage ) ) {
+            wikirank.init();
+            wikirank.exec();
+        }
         
     }
 
