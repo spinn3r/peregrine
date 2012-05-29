a15fe121c20d07c5d80020ca79e95802ed481436
burtonator <devnull@localhost>
2012-05-29 14:50:15 -0700

    sweet... wikipedia rank in its own class works...

--- src/test/peregrine/TestWikirank.java
+++ src/test/peregrine/TestWikirank.java
@@ -23,140 +23,19 @@ import peregrine.app.wikirank.*;
 
 public class TestWikirank extends peregrine.BaseTestWithMultipleProcesses {
 
-    public static int LIMIT = 10000;
-    
     @Override
     public void doTest() throws Exception {
 
         Config config = getConfig();
 
-        writeNodes( "corpus/wikirank/enwiki-20120502-page-sample.sql" );
-        writeLinks( "corpus/wikirank/enwiki-20120502-pagelinks-sample.sql" );
+        Wikirank wikirank = new Wikirank( config, 
+                                          "corpus/wikirank/enwiki-20120502-page-sample.sql",
+                                          "corpus/wikirank/enwiki-20120502-pagelinks-sample.sql" );
 
-        Controller controller = null;
-
-        try {
-
-            controller = new Controller( config );
-
-            // CreateNodeLookupjob
-            
-            controller.map( CreateNodeLookupJob.Map.class,
-                            new Input( "/wikirank/nodes" ),
-                            new Output( "shuffle:nodesByPrimaryKey",
-                                        "shuffle:nodesByHashcode" ) );
-
-            controller.reduce( Reducer.class,
-                               new Input( "shuffle:nodesByPrimaryKey" ),
-                               new Output( "/wikirank/nodesByPrimaryKey" ) );
-
-            controller.reduce( Reducer.class,
-                               new Input( "shuffle:nodesByHashcode" ),
-                               new Output( "/wikirank/nodesByHashcode" ) );
-                               
-            controller.map( FlattenLinksJob.Map.class,
-                            new Input( "/wikirank/links" ),
-                            new Output( "shuffle:default" ) );
-
-            controller.reduce( FlattenLinksJob.Reduce.class,
-                               new Input( "shuffle:default" ),
-                               new Output( "/wikirank/links.flattened" ) );
-
-            // this joins the node table AND the links table and then writes a
-            // raw hashcode graph for use with pagerank.
-            controller.merge( MergePagesAndLinksJob.Merge.class,
-                              new Input( "/wikirank/nodesByPrimaryKey",
-                                         "/wikirank/links.flattened" ),
-                              new Output( "/wikirank/graph" ) );
-
-            // the graph is written, now launch a job to finish it up.
-
-            Pagerank pr = new Pagerank( config, "/wikirank/graph", controller );
-            pr.exec( false );
-
-            //now join against nodesByPrimaryKey and and rank graph so that we can
-            //have rank per node
-
-            // merge /pr/out/rank_vector and nodesByPrimaryKey and node_metadata
-
-            controller.merge( MergeNodeAndRankMetaJob.Merge.class,
-                              new Input( "/pr/out/node_metadata",
-                                         "/pr/out/rank_vector",
-                                         "/wikirank/nodesByHashcode" ),
-                              new Output( "/wikirank/rank_metadata" ) );
-
-        } finally {
-            controller.shutdown();
-        }
-            
-    }
-
-    private void writeNodes( String input ) throws Exception {
-
-        Config config = getConfig();
-        
-        ExtractWriter writer = new ExtractWriter( config, "/wikirank/nodes" );
-
-        PageParser parser = new PageParser( input );
-
-        int index = 0;
-        
-        while( true ) {
-
-            PageParser.Page page = parser.next();
-
-            if ( page == null )
-                break;
-
-            ++index;
-
-            if ( index > LIMIT )
-                break;
-            
-            StructReader key = StructReaders.hashcode( "" + page.id );
-            StructReader value = StructReaders.wrap( page.name );
-            
-            writer.write( key , value );
-
-        }
-
-        writer.close();
-
-    }
-
-    private void writeLinks( String input ) throws Exception {
-
-        Config config = getConfig();
-        
-        ExtractWriter writer = new ExtractWriter( config, "/wikirank/links" );
-
-        PageLinkParser parser = new PageLinkParser( input );
-
-        int index = 0;
-        
-        while( true ) {
-
-            PageLinkParser.Link link = parser.next();
-
-            if ( link == null )
-                break;
-
-            ++index;
-
-            if ( index > LIMIT )
-                break;
-            
-            StructReader key = StructReaders.hashcode( "" + link.id );
-            StructReader value = StructReaders.wrap( link.name );
-            
-            writer.write( key , value );
-
-        }
-
-        writer.close();
+        wikirank.exec();
         
     }
-    
+
     public static void main( String[] args ) throws Exception {
 
         setPropertyDefault( "peregrine.test.config", "1:1:1" ); 
