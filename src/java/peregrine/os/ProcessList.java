646bd83e4c8daad50f6255a2f80a825319ba93e7
burtonator <devnull@localhost>
2012-06-19 16:21:32 -0700

    proceslist work

--- src/java/peregrine/os/ProcessList.java
+++ src/java/peregrine/os/ProcessList.java
@@ -56,6 +56,8 @@ public class ProcessList {
                     
                     String cmdline = new String( data );
 
+                    System.out.printf( "FIXME: %s\n", cmdline );
+                    
                     ProcessListEntry entry = new ProcessListEntry();
 
                     entry.setId( Integer.parseInt( current ) );
