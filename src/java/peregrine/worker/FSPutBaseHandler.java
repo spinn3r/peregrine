d7786f89288493155c3a3dfd7f50b1dce7bac758
burtonator <devnull@localhost>
2012-07-15 21:50:54 -0700

    revert to none and also keep two backup files for logs

--- src/java/peregrine/worker/FSPutBaseHandler.java
+++ src/java/peregrine/worker/FSPutBaseHandler.java
@@ -71,8 +71,8 @@ public class FSPutBaseHandler extends ErrorLoggingChannelUpstreamHandler {
 
             } else {
 
-                // log that this was written successfully including the NR of
-                // bytes.
+                // log that this was written successfully including the number
+                // of bytes.
 
                 long duration = System.currentTimeMillis() - started;
 
