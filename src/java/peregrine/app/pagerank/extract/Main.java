7897f1e93cc45fba460a920d6e3d35be3c0b104e
burtonator <devnull@localhost>
2012-09-26 14:23:54 -0700

    ...

--- src/java/peregrine/app/pagerank/extract/Main.java
+++ src/java/peregrine/app/pagerank/extract/Main.java
@@ -29,7 +29,7 @@ public class Main {
     public static void main( String[] args ) throws Exception {
 
         Config config = ConfigParser.parse( args );
-        new Initializer( config ).controller();
+        new Initializer( config ).basic( Main.class );
 
         Getopt getopt = new Getopt( args );
 
