bb900b2aa1fe8c38155efedfb485498d70cbfabe
burtonator <devnull@localhost>
2012-10-12 15:24:29 -0700

    added reporter so we can keep track of global job progress

---
+++ src/java/peregrine/worker/Reporter.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2011 Kevin A. Burton
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+package peregrine.worker;
+
+import java.util.*;
+
+import peregrine.config.partitioner.*;
+import peregrine.io.*;
+import peregrine.rpc.*;
+import peregrine.sort.*;
+import peregrine.util.*;
+import peregrine.controller.*;
+
+import com.spinn3r.log5j.Logger;
+
+/**
+ * A Reporter allows a job to report progress to the controller for tracking and
+ * auditing purposes.
+ */
+public class Reporter {
+
+    // the number of emitted records.  This should be ever increasing.
+    private Metric emitted = new Metric();
+
+    // the progress of our job between 0 and 100.
+    private Metric progress = new Metric( 100 );
+
+    public Metric getEmitted() {
+        return emitted;
+    }
+
+    public Metric getProgress() {
+        return progress;
+    }
+    
+    class Metric {
+
+        private long value;
+
+        private long max = Long.MAX_VALUE;
+
+        public Metric() {}
+
+        public Metric( long max ) {
+            this.max = max;
+        }
+
+        public void incr() {
+            ++value;
+        }
+
+        public long get() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return "" + value;
+        }
+        
+    }
+    
+}
