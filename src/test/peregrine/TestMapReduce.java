b5a944023849dc741318fc6b6e2f55031762e39c
burtonator <devnull@localhost>
2012-10-31 12:22:06 -0700

    two replicas are working ...

--- src/test/peregrine/TestMapReduce.java
+++ src/test/peregrine/TestMapReduce.java
@@ -254,7 +254,7 @@ public class TestMapReduce extends peregrine.BaseTestWithMultipleProcesses {
 
         //System.setProperty( "peregrine.test.config", "1:1:1" ); // 3sec
 
-        setPropertyDefault( "peregrine.test.factor", "1" ); // 
+        setPropertyDefault( "peregrine.test.factor", "100" ); // 
         setPropertyDefault( "peregrine.test.config", "1:2:3" ); // takes 3 seconds
 
         // 256 partitions... 
