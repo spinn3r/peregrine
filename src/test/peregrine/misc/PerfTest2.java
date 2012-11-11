2a9c847d9ec3cd17b893e5e30f027e07360e9ee3
burtonator <devnull@localhost>
2012-11-11 12:22:00 -0800

    another test of randomly changing various pages

---
+++ src/test/peregrine/misc/PerfTest2.java
@@ -0,0 +1,35 @@
+package peregrine.misc;
+
+import java.util.*;
+
+public class PerfTest2 {
+
+    public static void main( String[] args ) throws Exception {
+
+        long max = 1000L;
+        int range = 1000;
+
+        if ( args.length == 2 ) {
+            max = Integer.parseInt( args[0] );
+            range = Integer.parseInt( args[1] );
+        }
+        
+        System.out.printf( "max=%,d, range=%,d\n", max, range );
+
+        long v = 0;
+
+        byte[] data = new byte[range];
+
+        Random r = new Random();
+        
+        for( long i = 0; i < max; ++i ) {
+
+            int idx = r.nextInt( range );
+
+            ++data[idx];
+            
+        }
+
+    }
+    
+}
