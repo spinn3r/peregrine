e6265c427ed16b61798c8a2f9cb24a0ed7b8179e
burtonator <devnull@localhost>
2012-11-11 11:55:32 -0800

    perf tests

---
+++ src/test/peregrine/misc/PerfTest1.java
@@ -0,0 +1,17 @@
+package peregrine.misc;
+
+public class PerfTest1 {
+
+    public static void main( String[] args ) throws Exception {
+
+        long max = 10000000000L;
+
+        long v = 0;
+        
+        for( long i = 0; i < max; ++i ) {
+            ++v;
+        }
+
+    }
+    
+}
