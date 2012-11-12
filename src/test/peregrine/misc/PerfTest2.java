1f33cb19fab261aa23b8b66de7e6ad755c7b3a62
burtonator <devnull@localhost>
2012-11-11 16:13:25 -0800

    use slabs now

--- src/test/peregrine/misc/PerfTest2.java
+++ src/test/peregrine/misc/PerfTest2.java
@@ -7,26 +7,29 @@ public class PerfTest2 {
     public static void main( String[] args ) throws Exception {
 
         long max = 1000L;
+        int slabs = 1;
         int range = 1000;
 
         if ( args.length == 2 ) {
             max   = Long.parseLong( args[0] );
-            range = Integer.parseInt( args[1] );
+            slabs = Integer.parseInt( args[1] );
+            range = Integer.parseInt( args[2] );
         }
         
-        System.out.printf( "max=%,d, range=%,d\n", max, range );
+        System.out.printf( "max=%,d, slabs=%,d range=%,d\n", max, slabs, range );
 
         long v = 0;
 
-        byte[] data = new byte[range];
+        byte[][] data = new byte[slabs][range];
 
         Random r = new Random();
         
         for( long i = 0; i < max; ++i ) {
 
-            int idx = r.nextInt( range );
+            int slab_idx = r.nextInt( slabs );
+            int range_idx = r.nextInt( range );
 
-            ++data[idx];
+            ++data[slab_idx][range_idx];
             
         }
 
