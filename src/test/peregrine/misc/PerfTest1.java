889407dbb6cb92e5e18162d95bc42ca2344dcfca
burtonator <devnull@localhost>
2012-11-11 16:34:05 -0800

    print and load max

--- src/test/peregrine/misc/PerfTest1.java
+++ src/test/peregrine/misc/PerfTest1.java
@@ -6,6 +6,12 @@ public class PerfTest1 {
 
         long max = 100000000000L;
 
+        if ( args.length == 1 ) {
+            max = Long.parseLong( args[0] );
+        }
+
+        System.out.printf( "max: %,d\n", max );
+        
         long v = 0;
         
         for( long i = 0; i < max; ++i ) {
