0f24ed0f098adb3aa3b99f1dc66a6ae7745342af
burtonator <devnull@localhost>
2013-05-14 20:58:50 -0700

    big copyright update

--- src/java/peregrine/io/chunk/DefaultChunkReader.java
+++ src/java/peregrine/io/chunk/DefaultChunkReader.java
@@ -1,18 +1,18 @@
 /*
- * Copyright 2011 Kevin A. Burton
- * 
+ * Copyright 2011-2013 Kevin A. Burton
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 package peregrine.io.chunk;
 
 import java.io.*;
@@ -308,7 +308,7 @@ public class DefaultChunkReader extends BaseSSTableChunk
             //FIXME: keep looping through the requests because two clients might
             //have requested the same keys.
 
-            if ( find.getSeekKey().equals( key() ) ) {
+            if ( find.getSeekKey().equals(key()) ) {
 
                 listener.onRecord( find, key(), value() );
                 
