43205c7e915ecec1e22463bf9354d5ebe77a03a5
burtonator <devnull@localhost>
2013-05-25 23:32:27 -0700

    Fixed a bug where we overwrite build.xml every time

--- bin/misc/integrate.py
+++ bin/misc/integrate.py
@@ -35,7 +35,7 @@ BRANCH="default"
 SCRATCH="/tmp/integration/peregrine"
 TEST_LOGS="/var/lib/integration/peregrine"
 
-TEST_COMMAND="pkill -9 -u nobody java 2> /dev/null ; hg cat -r default build.xml > build.xml && export HOSTNAME=localhost && export ANT_OPTS=-Xmx512M && ant clean jar compile.test jar && time ant javadoc jxr && time ant test"
+TEST_COMMAND="pkill -9 -u nobody java 2> /dev/null ; && export HOSTNAME=localhost && export ANT_OPTS=-Xmx512M && ant clean jar compile.test jar && time ant javadoc jxr && time ant test"
 #TEST_COMMAND="false"
 
 ###
