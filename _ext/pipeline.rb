56406ae3eaab808b3fee8dfef2db27959ad81613
jasonkautz <jason@spinn3r.com>
2013-05-31 17:37:51 -0700

    removed template github page files and replaced with bitbucket peregrine website files

---
+++ _ext/pipeline.rb
@@ -0,0 +1,11 @@
+
+Awestruct::Extensions::Pipeline.new do
+
+  extension Awestruct::Extensions::Posts.new( '/blog' ) 
+  extension Awestruct::Extensions::Paginator.new(:posts, '/blog/index', :per_page => 5 )
+  extension Awestruct::Extensions::Atomizer.new(:posts, '/blog/index.atom')
+
+  helper Awestruct::Extensions::GoogleAnalytics
+
+end
+
